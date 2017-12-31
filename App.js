5cdd08f0d6ed5aa55e0dceb092f292e75b024bfa
Jahil Khalfe <jahil@bnry.digital>
2017-12-31 18:44:41 +0200

    Setting up configs

--- App.js
+++ App.js
@@ -2,22 +2,11 @@ import React from 'react';
 import styled from 'styled-components/native';
 import { StyleSheet, Text, View } from 'react-native';
 
-const StyledView = styled.View`
-  background-color: papayawhip;
-`;
-
-const StyledText = styled.Text`
-  color: palevioletred;
-`;
-
 export default class App extends React.Component {
   render() {
     return (
       <StyledView style={styles.container}>
         <StyledText>Open up App.js to start working on your app!</StyledText>
-        <StyledText>Shake your phone to open the developer menu.</StyledText>
-        <StyledText>Shake your phone to open the developer menu.</StyledText>
-        <StyledText>Shake your phone to open the developer menu.</StyledText>
       </StyledView>
     );
   }
